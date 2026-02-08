f8dde90309019c8e4a1e9bb90dda0238687fdbd6
Marvel Tech Hub <marvellousonyema32@gmail.com>
2026-02-08 08:10:22 -0800

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/upload", uploadRoute); // Upload route
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"], credentials: true },
+  cors: { origin: "https://portal-tumicodes.vercel.app/", methods: ["GET", "POST"], credentials: true },
 });
 
 app.set("io", io);
